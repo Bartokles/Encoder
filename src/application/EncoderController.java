7d987409a2f30b9028221e3337cd0c9a8a00a70c
Bartokles <bart.raczkowski@gmail.com>
2019-01-11 18:22:46 +0100

    Dodanie ComboBox'a

--- src/application/EncoderController.java
+++ src/application/EncoderController.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import encoder.TextEncoder;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class EncoderController {
+public class EncoderController implements Initializable {
 	@FXML
 	TextField encodeTextField;
+	
+	@FXML
+	private ComboBox<String> comboBox;
 	
 	private TextEncoder textEncoder;
 	
@@ -22,13 +30,19 @@ public class EncoderController {
 		String result = textEncoder.decode(text);
 		encodeTextField.setText(result);
 	}
-	//wm,kdjebhfgfre
 	@FXML
 	protected void handleEncodeButton(ActionEvent event)
 	{
 		String text = encodeTextField.getText();
 		String result = textEncoder.encode(text);
 		encodeTextField.setText(result);
+	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		String firstOption = " CesarEncoder ";
+		String secondOption = " AlfabetEncoder ";
+		comboBox.getItems().add(firstOption);
+		comboBox.getItems().add(secondOption);
 	}
 }
 
